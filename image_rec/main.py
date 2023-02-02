c2aaac5617b1dc0b45bad9bfba0aceb55768b60a
Jake Tucker <tuckerjake11@gmail.com>
2023-02-01 19:33:10 -0700

    base files

---
+++ image_rec/main.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+
+net = cv2.dnn.readNet('yolov3-tiny.weights', 'yolov3.cfg')
+classes = []
+with open('coco.names', 'r') as f:
+    classes = f.read().splitlines()
+
+print(classes)

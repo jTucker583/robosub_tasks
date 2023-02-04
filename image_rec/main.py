00edef684065c17438ef61117159ddc1aa921138
Jake Tucker <tuckerjake11@gmail.com>
2023-02-04 15:30:45 -0700

    ignoring all of the image training stuff because we dont need it

--- image_rec/main.py
+++ image_rec/main.py
@@ -1,10 +1,24 @@
 import cv2
 import numpy as np
 
+video = cv2.VideoCapture(0)
 
-net = cv2.dnn.readNet('yolov3-tiny.weights', 'yolov3.cfg')
-classes = []
-with open('coco.names', 'r') as f:
-    classes = f.read().splitlines()
-
-print(classes)
+while True:
+    ret, frame = video.read()
+    cv2.imshow("video",frame)
+    r = frame[:,:,2:]
+    g = frame[:,:,1:2]
+    b = frame[:,:,:1]
+    
+    #find the mean
+    r_mean = np.mean(r)
+    g_mean = np.mean(g)
+    b_mean = np.mean(b)
+    
+    #display the most prominant color:
+    if(b_mean > g_mean and g_mean > r_mean):
+        print("Blue")
+    elif(g_mean > b_mean and b_mean > r_mean):
+        print("Green")
+    else:
+        print("Red")
